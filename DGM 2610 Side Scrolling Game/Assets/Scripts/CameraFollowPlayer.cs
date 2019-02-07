98f927a628e050a5ca6263018f2205eb4b7b67c7
npbehunin <npbehunin@gmail.com>
2019-02-06 21:32:45 -0700

    More changes to level 1

--- DGM 2610 Side Scrolling Game/Assets/Scripts/CameraFollowPlayer.cs
+++ DGM 2610 Side Scrolling Game/Assets/Scripts/CameraFollowPlayer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class CameraFollowPlayer : MonoBehaviour
@@ -10,20 +9,12 @@ public class CameraFollowPlayer : MonoBehaviour
 
 	public float smoothSpeed = 0.125f;
 	public Vector3 offset;
-	public Vector3 actualPosition;
-	public float yPosition;
 
-	void Start()
-	{
-		yPosition = transform.position.y;
-	}
 	void FixedUpdate()
 	{
 		Vector3 desiredPosition = target.position + offset;
 		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		//transform.position = smoothedPosition;
-		actualPosition = new Vector3(smoothedPosition.x, smoothedPosition.y, smoothedPosition.z);
-		transform.position = actualPosition;
+		transform.position = smoothedPosition;
 
 
 		//transform.LookAt(target);
---
+++ DGM 2610 Side Scrolling Game/Assets/Scripts/floatData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewFloat", menuName = "AR/FloatData")] //Will let us create if we right click
+public class floatData : ScriptableObject
+{
+    public float value;
+
+    public virtual float Value //When this is called somewhere... get { return value; }
+    {
+        get { return value; } //We constantly want to get our horizontal or vertical
+    }
+}
