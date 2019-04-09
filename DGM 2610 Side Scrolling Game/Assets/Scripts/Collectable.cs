3a5959114c44666740f4c086a03aad7d1fcf3f52
npbehunin <npbehunin@gmail.com>
2019-04-08 18:29:33 -0600

    Added metal parts and lazergun with small interaction scripts

---
+++ DGM 2610 Side Scrolling Game/Assets/Scripts/Collectable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+
+	public bool islazergun;
+	public ParticleSystem system;
+
+	private Vector3 rotation;
+
+	void Update () 
+	{
+		gameObject.transform.Rotate(0, 180*Time.deltaTime, 0, Space.World);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.CompareTag("Player"))
+		{
+			Instantiate(system, transform.position, Quaternion.Euler(-90, 0, 0));
+			//system.Emit(1);
+			Destroy(gameObject);
+		}
+	}
+}
